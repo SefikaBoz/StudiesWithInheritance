b4fd35929c7235d4ff4bba4599fc1c7b35eac6da
SefikaBoz <sefikaboz12@gmail.com>
2017-05-18 21:47:26 +0300

    Update VirtualBaseClass.cs

--- VirtualBaseClass.cs
+++ VirtualBaseClass.cs
@@ -1,12 +1,10 @@
-# StudiesWithInheritance
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using TestlerDenemeler;
-namespace TestlerDenemeler
+using Test;
+namespace Test
 {
     class VirtualBaseClass
     {
