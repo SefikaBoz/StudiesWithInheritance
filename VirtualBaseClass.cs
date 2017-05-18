fb34e194f0b914ef5a52735c61e707473b567a5d
SefikaBoz <sefikaboz12@gmail.com>
2017-05-18 21:35:45 +0300

    Create VirtualBaseClass.cs

---
+++ VirtualBaseClass.cs
@@ -0,0 +1,51 @@
+# StudiesWithInheritance
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestlerDenemeler;
+namespace TestlerDenemeler
+{
+    class VirtualBaseClass
+    {
+        public virtual void BaseVirtualMethod()
+        {
+            Console.WriteLine("Base Class VirtualMethod");
+        }      
+    }
+    class DerivedVirtClasss : VirtualBaseClass
+    {
+        public override void BaseVirtualMethod()
+        {
+            Console.WriteLine("Derived Class VirtualMethod");
+        }
+    }
+    class EndClass : DerivedVirtClasss
+    {
+        public override void BaseVirtualMethod()
+        {
+            Console.WriteLine("EndClass Class VirtualMethod");
+        }
+    }
+    class End2Class : EndClass
+    {
+        public override void BaseVirtualMethod()
+        {
+            base.BaseVirtualMethod();
+        }
+    }
+    abstract class AbsClass
+    {
+        public abstract int MyProperty { get; set; }
+    }
+    class DerAbClass : AbsClass
+    {
+        public override int MyProperty
+        {
+            get { return 6; }
+            set { }
+        }
+    }
+}
